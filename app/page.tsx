3d20c20fad0e9f0b6d3eb31d87ca98f3a0af8968
Marcoant007 <marcoantnovo@gmail.com>
2024-12-14 18:41:20 -0300

    chore: add shadcn setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
+import { Button } from "./_components/ui/button";
 
 export default function Home() {
-  return <h1 className="text-red-500"></h1>
+  return <Button>Hello world</Button>
 }
