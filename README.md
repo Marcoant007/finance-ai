756ceb85f95b0665d599c3a3a0950846604f19a8
Marcoant007 <marcoantnovo@gmail.com>
2024-12-14 19:55:00 -0300

    feat: add husky

--- README.md
+++ README.md
@@ -3,3 +3,10 @@ Repo: https://github.com/felipemotarocha/fullstackclub-finance-dashboard-api
 // opções do auth
 clerk
 next auth
+
+githooks
+shadcn
+taiwindcss
+
+husky
+lint staged
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -12,17 +12,22 @@ const LoginPage = async () => {
   }
 
   return (
-
     <div className="grid h-full grid-cols-2">
-      <div className="flex flex-col h-full justify-center p-8 max-w-[550px] mx-auto">
-        <Image src="/logo.svg" width={173} height={39} alt="Finance AI" className="mb-8" />
-        <h1 className="text-4xl font-bold mb-3">Bem-vindo</h1>
+      <div className="mx-auto flex h-full max-w-[550px] flex-col justify-center p-8">
+        <Image
+          src="/logo.svg"
+          width={173}
+          height={39}
+          alt="Finance AI"
+          className="mb-8"
+        />
+        <h1 className="mb-3 text-4xl font-bold">Bem-vindo</h1>
         <p className="mb-8 text-muted-foreground">
           A Finance AI é uma plataforma de gestão financeira que utiliza IA para
           monitorar suas movimentações, e oferecer insights personalizados,
           facilitando o controle de seu orçamento
         </p>
-        <SignInButton >
+        <SignInButton>
           <Button variant="outline">
             <LogInIcon className="mr-2" />
             Fazer Login ou criar conta
@@ -30,10 +35,15 @@ const LoginPage = async () => {
         </SignInButton>
       </div>
       <div className="relative h-full w-full">
-        <Image src="/login.png" alt="Faça login" fill className="object-cover" />
+        <Image
+          src="/login.png"
+          alt="Faça login"
+          fill
+          className="object-cover"
+        />
       </div>
     </div>
   );
-}
+};
 
 export default LoginPage;
